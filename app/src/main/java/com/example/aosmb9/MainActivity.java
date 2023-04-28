808099298642116053346c6c02d52712e1fd1e9b
Dany <lebedevdani2003@yandex.ru>
2023-04-28 20:47:18 +0300

    1 point done

--- app/src/main/java/com/example/aosmb9/MainActivity.java
+++ app/src/main/java/com/example/aosmb9/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.aosmb9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sending;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        sending = (Button) findViewById(R.id.sendbtn);
+
+        sending.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = "тестовый текст";
+                Intent intent = new Intent(Intent.ACTION_SEND);
+
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, text);
+
+
+                startActivity(Intent.createChooser(intent, "информация"));
+            }
+
+        });
     }
 }
