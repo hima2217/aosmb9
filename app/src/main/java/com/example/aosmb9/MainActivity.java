5f1f271a3f7b325cf03425ea71ccaadbe0cde7df
Dany <lebedevdani2003@yandex.ru>
2023-04-28 21:29:56 +0300

    second point done

--- app/src/main/java/com/example/aosmb9/MainActivity.java
+++ app/src/main/java/com/example/aosmb9/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.aosmb9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button sending;
+    TextView text;
 
 
     @Override
@@ -34,5 +37,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+                if (sharedText != null) {
+                    TextView text = findViewById(R.id.inform);
+                    text.setText(sharedText);
+                }
+            }
+        }
     }
 }
